005b16ba3ebb74ad36b4d524e38bcb829399aa3f
TotalBollocks <jqrainwater@yahoo.com>
2014-04-30 20:38:10 -0500

    first ci

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestTester::Application.config.secret_key_base = 'a6657a670e711e85a9d59de120a9fcf41b6ec92fe074c830a9d22411852ab86f92deefe770b0d00cc90a5f580ee15614702780acf0fa0c51625961976975807c'
